c34ed7846050c462d150176d242f2386c9f99f72
Your Name <your.email@example.com>
2025-03-13 16:33:43 -0700

    resnetv2 v17

--- train_resnet18_subset.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Training on 10% of the full dataset"
-python main.py --cfg configs/resnet18_base.yaml --subset-fraction 0.1 --opts DATA.BATCH_SIZE 256 DATA.NUM_WORKERS 32 TRAIN.EPOCHS 20 
